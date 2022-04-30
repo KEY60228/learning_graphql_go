523ac1cd0a78e3f1ac4d7031e783b1b661c455a8
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-04-30 21:05:28 +0900

    modify resolvers

--- server/graph/schema.resolvers.go
+++ server/graph/schema.resolvers.go
@@ -6,39 +6,58 @@ package graph
 import (
 	"context"
 	"fmt"
-	"gql/graph/generated"
-	"gql/graph/model"
 	"strconv"
 	"time"
+
+	"gql/graph/generated"
+	"gql/graph/model"
 )
 
 func (r *mutationResolver) PostPhoto(ctx context.Context, input model.PostPhotoInput) (*model.Photo, error) {
 	ID++
+
 	photo := &model.Photo{
 		ID:          strconv.Itoa(ID),
-		Name:        input.Name,
 		URL:         fmt.Sprintf("https://example.com/img/%d.jpg", ID),
+		Name:        input.Name,
 		Description: input.Description,
 		Category:    *input.Category,
+		PostedBy:    getUserByGithubLogin("mHattrup"),
+		Created:     model.DateTime(time.Now()),
 	}
 	Photos = append(Photos, photo)
+
+	tag := []struct {
+		PhotoID string
+		UserID  string
+	}{{
+		PhotoID: strconv.Itoa(ID),
+		UserID:  "mHattrup",
+	}, {
+		PhotoID: strconv.Itoa(ID),
+		UserID:  "gPlake",
+	}}
+	Tags = append(Tags, tag...)
+
 	return photo, nil
 }
 
 func (r *queryResolver) TotalPhotos(ctx context.Context) (int, error) {
-	for _, tag := range Tags {
-		for _, photo := range Photos {
-			if tag.PhotoID == photo.ID {
-				photo.TaggedUsers = append(photo.TaggedUsers, getUserByGithubLogin(tag.UserID))
-			}
-		}
-	}
-
 	return len(Photos), nil
 }
 
 func (r *queryResolver) AllPhotos(ctx context.Context) ([]*model.Photo, error) {
-	return Photos, nil
+	photos := make([]*model.Photo, len(Photos))
+	for i, photo := range Photos {
+		copy := *photo
+		for _, tag := range Tags {
+			if tag.PhotoID == copy.ID {
+				copy.TaggedUsers = append(copy.TaggedUsers, getUserByGithubLogin(tag.UserID))
+			}
+		}
+		photos[i] = &copy
+	}
+	return photos, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
@@ -65,64 +84,58 @@ var Tags []struct {
 }
 
 func init() {
-	if len(Users) == 0 {
-		Users = []*model.User{
-			{GithubLogin: "mHattrup", Name: "Mike Hattrup"},
-			{GithubLogin: "gPlake", Name: "Glen Plake"},
-			{GithubLogin: "sSchmidt", Name: "Scot Schmidt"},
-		}
-	}
-
-	if len(Photos) == 0 {
-		Photos = []*model.Photo{
-			{
-				ID:          "1",
-				Name:        "Dropping the Heart Chute",
-				Description: toPtr("The heart chute is one of my favorite chutes"),
-				Category:    model.PhotoCategoryAction,
-				PostedBy:    getUserByGithubLogin("gPlake"),
-				Created:     model.DateTime(stringToTime("1977/3/28")),
-			},
-			{
-				ID:       "2",
-				Name:     "Enjoying the sunshine",
-				Category: model.PhotoCategorySelfie,
-				PostedBy: getUserByGithubLogin("sSchmidt"),
-				Created:  model.DateTime(stringToTime("1985/1/24")),
-			},
-			{
-				ID:          "3",
-				Name:        "Gunbarrel 25",
-				Description: toPtr("25 laps on gunbarrel today"),
-				Category:    model.PhotoCategoryLandscape,
-				PostedBy:    getUserByGithubLogin("sSchmidt"),
-				Created:     model.DateTime(stringToTime("2018/04/15")),
-			},
-		}
-	}
-
-	if len(Tags) == 0 {
-		Tags = []struct {
-			PhotoID string
-			UserID  string
-		}{
-			{PhotoID: "1", UserID: "gPlake"},
-			{PhotoID: "2", UserID: "sSchmidt"},
-			{PhotoID: "3", UserID: "mHattrup"},
-			{PhotoID: "4", UserID: "gPlake"},
-		}
+	Users = []*model.User{
+		{GithubLogin: "mHattrup", Name: "Mike Hattrup"},
+		{GithubLogin: "gPlake", Name: "Glen Plake"},
+		{GithubLogin: "sSchmidt", Name: "Scot Schmidt"},
 	}
 }
-
+func init() {
+	Photos = []*model.Photo{
+		{
+			ID:          "1",
+			Name:        "Dropping the Heart Chute",
+			Description: toPtr("The heart chute is one of my favorite chutes"),
+			Category:    model.PhotoCategoryAction,
+			PostedBy:    getUserByGithubLogin("gPlake"),
+			Created:     model.DateTime(stringToTime("1977/3/28")),
+		},
+		{
+			ID:       "2",
+			Name:     "Enjoying the sunshine",
+			Category: model.PhotoCategorySelfie,
+			PostedBy: getUserByGithubLogin("sSchmidt"),
+			Created:  model.DateTime(stringToTime("1985/1/24")),
+		},
+		{
+			ID:          "3",
+			Name:        "Gunbarrel 25",
+			Description: toPtr("25 laps on gunbarrel today"),
+			Category:    model.PhotoCategoryLandscape,
+			PostedBy:    getUserByGithubLogin("sSchmidt"),
+			Created:     model.DateTime(stringToTime("2018/04/15")),
+		},
+	}
+	ID += len(Photos)
+}
+func init() {
+	Tags = []struct {
+		PhotoID string
+		UserID  string
+	}{
+		{PhotoID: "1", UserID: "gPlake"},
+		{PhotoID: "2", UserID: "sSchmidt"},
+		{PhotoID: "2", UserID: "mHattrup"},
+		{PhotoID: "2", UserID: "gPlake"},
+	}
+}
 func toPtr(s string) *string {
 	return &s
 }
-
 func stringToTime(s string) time.Time {
 	t, _ := time.Parse("2006/1/2", s)
 	return t
 }
-
 func getUserByGithubLogin(s string) *model.User {
 	for _, user := range Users {
 		if user.GithubLogin == s {
