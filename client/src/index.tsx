f702b8156e7a33fdeff8d25cdf437f7e33d22bff
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-05-06 15:13:37 +0900

    install apollo-client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider, ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+
+const link = new HttpLink({
+  uri: "http://localhost:8080/query",
+  headers: {
+    authorization: "e753ebecac81ade72470c507d0de84484dc84ca9"
+  }
+})
+const cache = new InMemoryCache()
+const client = new ApolloClient({link, cache})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
