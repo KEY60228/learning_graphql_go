d322267e2b2ce4b521cc07b493a5ef0a02a1d0f2
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-05-14 20:23:56 +0900

    7-1-2-1. WebSocketLinkの追加

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import { ApolloProvider, ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloProvider, ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { createClient } from 'graphql-ws'
 import { persistCache, LocalStorageWrapper } from 'apollo3-cache-persist';
 
 import App from './App';
+import { getMainDefinition } from '@apollo/client/utilities';
 
 const link = new HttpLink({
   uri: "http://localhost:8080/query",
@@ -12,12 +15,24 @@ const link = new HttpLink({
     authorization: localStorage.getItem("token")
   }
 })
+const wsLink = new GraphQLWsLink(createClient({
+  url: 'ws://localhost:8080/query'
+}))
+const splitLink = split(({query}) => {
+  const definition = getMainDefinition(query)
+  return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+}, wsLink, link)
+
 const cache = new InMemoryCache()
 persistCache({
   cache,
   storage: new LocalStorageWrapper(localStorage),
 })
-const client = new ApolloClient({link, cache})
+
+const client = new ApolloClient({
+  link: splitLink,
+  cache
+})
 
 if (localStorage['apollo-cache-persist']) {
   const cacheData = JSON.parse(localStorage['apollo-cache-persist'])
