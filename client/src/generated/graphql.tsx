7628bc4ce076e89aaee44665c99aac17f7c49426
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-05-14 20:23:40 +0900

    generate graphql.tsx

--- client/src/generated/graphql.tsx
+++ client/src/generated/graphql.tsx
@@ -80,6 +80,22 @@ export type Query = {
   totalUsers: Scalars['Int'];
 };
 
+export type Subscription = {
+  __typename?: 'Subscription';
+  newPhoto: Photo;
+  newUsers: Array<User>;
+};
+
+
+export type SubscriptionNewPhotoArgs = {
+  githubLogin: Scalars['String'];
+};
+
+
+export type SubscriptionNewUsersArgs = {
+  githubLogin: Scalars['String'];
+};
+
 export type User = {
   __typename?: 'User';
   avatar: Scalars['String'];
@@ -112,6 +128,20 @@ export type AddFakeUsersMutationVariables = Exact<{
 
 
 export type AddFakeUsersMutation = { __typename?: 'Mutation', addFakeUsers: Array<{ __typename?: 'User', githubLogin: string, name: string, avatar: string }> };
+
+export type NewUsersSubscriptionVariables = Exact<{
+  githubLogin: Scalars['String'];
+}>;
+
+
+export type NewUsersSubscription = { __typename?: 'Subscription', newUsers: Array<{ __typename?: 'User', githubLogin: string, name: string, avatar: string }> };
+
+export type NewPhotoSubscriptionVariables = Exact<{
+  githubLogin: Scalars['String'];
+}>;
+
+
+export type NewPhotoSubscription = { __typename?: 'Subscription', newPhoto: { __typename?: 'Photo', url: string, category: PhotoCategory, postedBy: { __typename?: 'User', githubLogin: string, name: string, avatar: string } } };
 
 export const UserInfoFragmentDoc = gql`
     fragment userInfo on User {
@@ -259,3 +289,71 @@ export function useAddFakeUsersMutation(baseOptions?: Apollo.MutationHookOptions
 export type AddFakeUsersMutationHookResult = ReturnType<typeof useAddFakeUsersMutation>;
 export type AddFakeUsersMutationResult = Apollo.MutationResult<AddFakeUsersMutation>;
 export type AddFakeUsersMutationOptions = Apollo.BaseMutationOptions<AddFakeUsersMutation, AddFakeUsersMutationVariables>;
+export const NewUsersDocument = gql`
+    subscription newUsers($githubLogin: String!) {
+  newUsers(githubLogin: $githubLogin) {
+    githubLogin
+    name
+    avatar
+  }
+}
+    `;
+
+/**
+ * __useNewUsersSubscription__
+ *
+ * To run a query within a React component, call `useNewUsersSubscription` and pass it any options that fit your needs.
+ * When your component renders, `useNewUsersSubscription` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the subscription, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useNewUsersSubscription({
+ *   variables: {
+ *      githubLogin: // value for 'githubLogin'
+ *   },
+ * });
+ */
+export function useNewUsersSubscription(baseOptions: Apollo.SubscriptionHookOptions<NewUsersSubscription, NewUsersSubscriptionVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useSubscription<NewUsersSubscription, NewUsersSubscriptionVariables>(NewUsersDocument, options);
+      }
+export type NewUsersSubscriptionHookResult = ReturnType<typeof useNewUsersSubscription>;
+export type NewUsersSubscriptionResult = Apollo.SubscriptionResult<NewUsersSubscription>;
+export const NewPhotoDocument = gql`
+    subscription newPhoto($githubLogin: String!) {
+  newPhoto(githubLogin: $githubLogin) {
+    url
+    category
+    postedBy {
+      githubLogin
+      name
+      avatar
+    }
+  }
+}
+    `;
+
+/**
+ * __useNewPhotoSubscription__
+ *
+ * To run a query within a React component, call `useNewPhotoSubscription` and pass it any options that fit your needs.
+ * When your component renders, `useNewPhotoSubscription` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the subscription, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useNewPhotoSubscription({
+ *   variables: {
+ *      githubLogin: // value for 'githubLogin'
+ *   },
+ * });
+ */
+export function useNewPhotoSubscription(baseOptions: Apollo.SubscriptionHookOptions<NewPhotoSubscription, NewPhotoSubscriptionVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useSubscription<NewPhotoSubscription, NewPhotoSubscriptionVariables>(NewPhotoDocument, options);
+      }
+export type NewPhotoSubscriptionHookResult = ReturnType<typeof useNewPhotoSubscription>;
+export type NewPhotoSubscriptionResult = Apollo.SubscriptionResult<NewPhotoSubscription>;
